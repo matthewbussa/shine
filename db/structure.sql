f32316a4dba44340380d6267cc942d5151730007
Matthew Bussa <matthewbussa@gmail.com>
2016-04-27 22:58:08 -0400

    ability to search for customers

--- db/structure.sql
+++ db/structure.sql
@@ -148,6 +148,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -193,4 +214,6 @@ INSERT INTO schema_migrations (version) VALUES ('20160423183401');
 INSERT INTO schema_migrations (version) VALUES ('20160426205828');
 
 INSERT INTO schema_migrations (version) VALUES ('20160427013454');
+
+INSERT INTO schema_migrations (version) VALUES ('20160429003915');
 
