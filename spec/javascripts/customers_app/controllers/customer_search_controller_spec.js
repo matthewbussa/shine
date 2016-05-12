5c943149ed29f5c7d8b47608ac09b4c29e1d0a6e
Matthew Bussa <matthewbussa@gmail.com>
2016-05-11 22:54:07 -0400

    mock http backend angular test

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -1,21 +1,54 @@
 describe("CustomerSearchController", function() {
-  describe("Initialization", function() {
+    describe("Initialization", function() {
 
-    var scope = null;
-    var controller = null;
+        var scope = null;
+        var controller = null;
 
-    beforeEach(module("customers"));
+        beforeEach(module("customers"));
 
-    beforeEach(inject(function($controller, $rootScope) {
-      scope = $rootScope.$new();
-      controller = $controller("CustomerSearchController", {
-        $scope: scope
-      });
-    }));
+        beforeEach(inject(function($controller, $rootScope) {
+            scope = $rootScope.$new();
+            controller = $controller("CustomerSearchController", {
+                $scope: scope
+            });
+        }));
 
-    it("defaults to an empty customer list", function() {
-      expect(scope.customers).toEqualData([]);
+        it("defaults to an empty customer list", function() {
+            expect(scope.customers).toEqualData([]);
+        });
+
     });
+});
 
+describe("Fetching Search Results"​, ​function​() {​ ​
+  var​ scope = ​null​,
+      ​controller = ​null​,
+      httpBackend = ​null​,
+      ​serverResults = [​
+      {​
+        id: 123, ​ 
+        first_name: "Bob"​,
+        last_name: "Jones"​,
+        email: "bjones@ foo.net"​,
+        username: "jonesy"​​
+      },
+      {​
+        id: 456,
+        first_name: "Bob"​,
+        last_name: "Johnsons"​,
+        email: "johnboy@ bar.info"​,
+        username: "bobbyj"​​
+      }​
+    ];
+  beforeEach(inject(​function​($ controller, $ rootScope, $ httpBackend) {​ 
+      scope = $ rootScope.$new();
+      httpBackend = $ httpBackend;​ 
+      controller = $ controller(​" CustomerSearchController"​, {​ $scope: scope​ });​
+  }));
+
+  it("populates the customer list with the results", function() {
+    scope.search("bob");
+    httpBackend.flush();
+    expect(scope.customers).toEqualData(serverResults);
   });
 });
