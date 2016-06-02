0da7f567e5ddc20bfe0d6a1104ee8ebe9b6105af
Matthew Bussa <matthewbussa@gmail.com>
2016-06-02 12:51:59 -0400

    added tests for credit card controller

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -56,9 +56,9 @@ app.controller("CustomerSearchController", [
 app.controller("CustomerDetailController", [
   "$scope", "$routeParams", "$resource",
   function($scope, $routeParams, $resource){
-    var customerId = $routeParams.id;
+    $scope.customerId = $routeParams.id;
     var Customer = $resource('/customers/:customerId.json');
-    $scope.customer = Customer.get( { "customerId": customerId } );
+    $scope.customer = Customer.get( { "customerId": $scope.customerId } );
   }
 ]);
 
@@ -66,6 +66,8 @@ app.controller("CustomerCreditCardController", [
   "$scope","$resource",
   function($scope, $resource) {
     var CreditCardInfo = $resource('/fake_billing.json');
-    $scope.creditCard = CreditCardInfo.get({"cardholder_id": 1234});
+    $scope.setCardholderId = function(cardholderId) {
+      $scope.creditCard = CreditCardInfo.get({"cardholder_id": cardholderId})
+    }
   }
 ]);
