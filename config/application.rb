b17a1a680313d17304575185966e7d948273c302
Matthew Bussa <matthewbussa@gmail.com>
2016-06-16 13:32:21 -0400

    add and configure bootstrap icones and fonts

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,14 @@ module Shine
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_record.schema_format = :sql
+
+    config.assets.paths << Rails.root.join("vendor",
+        "assets",
+        "bower_components",
+        "bootstrap-sass-official",
+        "assets",
+        "fonts")
+
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)\z/
   end
 end
